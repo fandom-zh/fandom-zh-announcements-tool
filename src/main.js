c28357a546318e13335ea52b51406b26ec8647c7
Dianliang233 <dianliang233@gmail.com>
2021-05-16 12:52:45 +0800

    🚸 PWA

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onOfflineReady() {
+    // show a ready to work offline to user
+  },
+})
 
 createApp(App).mount('#app')
